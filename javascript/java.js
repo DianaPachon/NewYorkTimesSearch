2180477b20d9c327c000517cc61e8a3b3080f1bd
cfern052 <cfern052@gmail.com>
2019-01-12 14:08:26 -0500

    update before end of class

--- javascript/java.js
+++ javascript/java.js
@@ -1,21 +1,36 @@
-function displayArticles() {
-    
-    var userInput='';
-    var recordInput='';
-    var article = $(this).attr("data-name");
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + recordInput + "&apikey=844c969fc7e244009d5ac461b7e42f6e";
+$(document).ready(function () {
+var userInput; 
+var recordInput;
+var queryURL;
+    function displayArticles() {
 
-    // Creates AJAX call for the specific movie button being clicked
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-        var responseArray= response.docs;
-        for(var i=0; i<userInput; i++){
-            responseArray[i].web_url
-            responseArray[i].snippet
-            responseArray[i].headline.main
-        }
+        userInput = $("#exampleInputEmail1").val().trim();
+        recordInput = parseInt($("#exampleInputPassword1").val());
+
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + "&apikey=b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+
+        // Creates AJAX call for the specific movie button being clicked
+        return queryURL;
 
     }
-       
+    $('.btn-primary').on("click", function () {
+
+        event.preventDefault();
+
+        var queryURL = displayArticles();
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var responseArray = response.docs;
+            for (var i = 0; i < 5; i++) {
+                // responseArray[i].web_url;
+                // responseArray[i].snippet;
+                // responseArray[i].headline.main;
+                console.log(responseArray[i].web_url, responseArray[i].snippet, responseArray[i].headline.main);
+            }
+
+        })
+    })
+
+});
